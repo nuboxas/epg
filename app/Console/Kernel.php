cf5d23e8e924f9813261e7ae17212e30316e55f0
Marius <marius@marius.ovh>
2020-09-07 21:55:01 +0100

    schedule at 2:30 UTC

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new \App\Jobs\downloadEPG)->everyMinute();
+        $schedule->job(new \App\Jobs\downloadEPG)->dailyAt('2:30');
     }
 
     /**
