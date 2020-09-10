95f6705df1567633e4568c86cfa35873351c027a
MariusWBS <marius.zurbenka@worldwidebookservices.com>
2020-09-10 09:01:45 +0100

    added migrations

---
+++ database/migrations/2020_09_10_075403_create_epg_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEpgTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('epg', function (Blueprint $table) {
+            $table->string('id', 36);
+            $table->foreignId('provider_id')->constrained('providers');
+            $table->string('tvg_id', 60);
+            $table->string('name', 255);
+            $table->integer('time_from');
+            $table->integer('time_to');
+            $table->text('description')->nullable();
+            $table->timestamps();
+            //Creating Index
+            $table->index('tvg_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('epg');
+    }
+}
