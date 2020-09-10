0d376e0b2b12fd8d64e183124e5cb20487e83554
MariusWBS <marius.zurbenka@worldwidebookservices.com>
2020-09-10 12:43:20 +0100

    added artisan command to manually trigger downloadEPG job

---
+++ app/Console/Commands/DispatchJob.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DispatchJob extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'epg:download';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Download EPG manually';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        \App\Jobs\downloadEPG::dispatch();
+    }
+}
